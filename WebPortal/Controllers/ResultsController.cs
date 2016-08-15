891efbad440b7b694f7a054517168b09719bf9f0
jelaaato <erjel88@yahoo.com>
2016-08-15 09:38:13 +0800

    modal fixes

--- WebPortal/Controllers/ResultsController.cs
+++ WebPortal/Controllers/ResultsController.cs
@@ -35,91 +35,13 @@ namespace WebPortal.Controllers
             DirectoryInfo dir = new DirectoryInfo(path);
             int period = db.PortalRetention.Where(a => a.Module == "Results").Select(a => a.Retention_Period).First();
             DateTime minDate = DateTime.Today.AddDays(period);
-            rmodel.PDFFile = dir.GetFiles("*.pdf*").Where(a => a.Name.Contains(samplecurrentuser) && (a.CreationTime > minDate));
+            rmodel.PDFFile = dir.GetFiles("*.pdf*").Where(a => a.Name.Contains(samplecurrentuser) && (a.CreationTime > minDate)).OrderByDescending(a => a.CreationTime);
             rmodel.retentionperiod = period * -1;
             rmodel.fileid = fileid;
             rmodel.IsValidated = isvalidated;
             
             return View(rmodel);
         }
-
-        //[HttpPost]
-        //public ActionResult Validate(ResultModel model, int fileID)
-        //{
-        //    var user = UserManager.Find(User.Identity.Name, model.Password);
-        //    if (user != null)
-        //    {
-        //        model.IsValidated = true;
-        //        if (model.IsValidated == true)
-        //        {
-        //            Session["Validated"] = "yes";
-        //            Session["fileid"] = model.fileid;
-        //        }
-        //        else
-        //        {
-        //            Session["Validated"] = "no";
-        //        }
-
-        //        return RedirectToAction("Laboratory", "Results", model);
-        //    }
-        //    else
-        //    {
-        //        model.IsValidated = false;
-
-        //        return Redirect("/Results/Laboratory");
-        //    }
-
-        //}
-
-        //[Authorize]
-        //public ActionResult UpdateRetention()
-        //{
-        //    return View();
-        //}
-
-        //[Authorize]
-        //[HttpPost]
-        //public ActionResult UpdateRetention(ResultModel model)
-        //{
-        //    if (db.PortalResultsRetention.Any(a => a.UserName == HttpContext.User.Identity.Name))
-        //    {
-        //        if ((model.retentionperiod <= 0) || (model.retentionperiod > 30))
-        //        {
-        //            @ViewBag.Error = "Please enter a value greater than 0 and less than 30 days.";
-        //        }
-        //        else
-        //        {
-        //            PortalResultsRetention user = db.PortalResultsRetention.First(a => a.UserName == HttpContext.User.Identity.Name);
-        //            user.Retention_Period = model.retentionperiod * -1;
-        //            db.SaveChanges();
-        //            return RedirectToAction("Laboratory", "Results");
-        //        }
-        //    }
-        //    else
-        //    {
-        //        if ((model.retentionperiod <= 0) || (model.retentionperiod > 30))
-        //        {
-        //            @ViewBag.Error = "Please enter a value greater than 0 and less than 30 days.";
-        //        }
-        //        else
-        //        {
-        //            int period = model.retentionperiod * -1;
-        //            PortalResultsRetention retention = new PortalResultsRetention()
-        //            {
-        //                User_Id = HttpContext.User.Identity.GetUserId(),
-        //                UserName = HttpContext.User.Identity.Name,
-        //                Retention_Period = period,
-        //            };
-
-        //            db.PortalResultsRetention.Add(retention);
-        //            db.SaveChanges();
-        //            return RedirectToAction("Laboratory", "Results");
-        //        }
-       
-        //    }
-        //    return View(model);
-            
-        //}
 
         [Authorize]
         public ActionResult Radiology()
